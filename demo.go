5b1343e074beba2d08087759bf5485ec3b217d42
chinarush <xzbrush@163.com>
2021-04-27 10:54:40 +0800

    this commit is to make this repo more convient to all users C

---
+++ demo.go
@@ -0,0 +1,26 @@
+package main
+import "fmt"
+
+func main(){
+	var a ,b string = 3, 4
+	fmt.Println("the value of a and b before swap are %d\n %d\n", a,b)
+
+	swap(a,b)
+	fmt.Println("the value of a and b after swap are %d\n %d\n", a,b)
+}
+
+func swap(x,y string)[string, string]{
+
+	return y,x
+
+}
+
+	func sum_and_cha(a int, b int)(int, int){
+
+	sum := a+b
+	sub := a- b
+	return sum ,sub
+
+}
+
+
